da09bbd06e105c1ff716b4dae049f94290f68039
Lospub <zhjyy002@gmail.com>
2025-01-21 04:31:25 -0700

    rename budget controller route from 'budget' to 'budgets'

--- src/budget/budget.controller.ts
+++ src/budget/budget.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { BudgetService } from './budget.service';
 import { Budget } from './entities/budget.entity';
 
-@Controller('budget')
+@Controller('budgets')
 export class BudgetController {
     constructor(private readonly budgetService: BudgetService) {}
 
